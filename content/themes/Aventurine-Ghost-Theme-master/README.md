8b96064a9d98733dc5476041b2b2390636ff0127
Lingzi Jin <lingzi.jin@boostinsider.com>
2015-06-18 19:34:38 -0700

    add new file to git

---
+++ content/themes/Aventurine-Ghost-Theme-master/README.md
@@ -0,0 +1,11 @@
+# Aventurine Ghost Theme
+
+A minimal [Ghost](http://github.com/tryghost/ghost/) freebie to get your first blog up and running beautifully.
+
+Live Preview: [http://aventurine.crystalline-themes.com](http://aventurine.crystalline-themes.com)
+
+## Copyright & License
+
+© 2014 Crystalline Themes. All rights reserved.
+
+Licensed under [Creative Commons Attribution 3.0](https://creativecommons.org/licenses/by/3.0/us/).
---
+++ content/themes/Aventurine-Ghost-Theme-master/assets/js/init.js
@@ -0,0 +1,105 @@
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+
+			// Global.
+				global: {
+					href: '/assets/css/style.css',
+                    containers: 1400,
+					grid: {
+						gutters: ['3em', '1.5em']
+					}
+				},
+
+			// XLarge.
+				xlarge: {
+					media: '(max-width: 1680px)',
+                    containers: 1260,
+					href: '/assets/css/style-xlarge.css',
+				},
+
+			// Large.
+				large: {
+					media: '(max-width: 1280px)',
+					href: '/assets/css/style-large.css',
+                    containers: 960,
+					viewport: {
+						scalable: false
+					}
+				},
+
+			// Medium.
+				medium: {
+					media: '(max-width: 980px)',
+					href: '/assets/css/style-medium.css',
+					containers: '90%',
+					grid: {
+						zoom: 2
+					}
+				},
+
+			// Small.
+				small: {
+					media: '(max-width: 736px)',
+					href: '/assets/css/style-small.css',
+					containers: '90%',
+					grid: {
+						zoom: 3,
+						gutters: ['1.25em', 0]
+					}
+				},
+
+			// XSmall.
+				xsmall: {
+					media: '(max-width: 480px)',
+					href: '/assets/css/style-xsmall.css',
+					grid: {
+						zoom: 4
+					}
+				}
+
+		},
+		plugins: {
+			layers: {
+
+				// Config.
+					config: {
+						transform: true
+					},
+
+				// Navigation Panel.
+					navPanel: {
+						animation: 'pushY',
+						breakpoints: 'medium',
+						clickToHide: true,
+						height: '100%',
+						hidden: true,
+						html: '<div data-action="moveElement" data-args="nav"></div>',
+						orientation: 'vertical',
+						position: 'top',
+						side: 'top',
+						width: '100%',
+                        height: '30%',
+					},
+
+				// Navigation Button.
+					navButton: {
+						breakpoints: 'medium',
+						height: '1.75em',
+						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+						position: 'top',
+						side: 'top',
+						width: '3.5em'
+					}
+
+			}
+		}
+	});
+
+	$(function() {
+
+	});
+
+})(jQuery);
